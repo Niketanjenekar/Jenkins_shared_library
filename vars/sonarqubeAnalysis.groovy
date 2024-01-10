4771bec94d54be429dd27405644b4a366ee88749
Niketan Jenekar <97117476+Niketanjenekar@users.noreply.github.com>
2024-01-10 21:50:04 +0530

    agin same correction

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
-    withSonarQubeEnv('sonar') {
+    withSonarQubeEnv('sonar-server') {
         sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Youtube-clone -Dsonar.projectKey=Youtube-clone '''
     }
 }
